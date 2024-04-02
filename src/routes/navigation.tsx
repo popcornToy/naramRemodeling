f60310df2c188d02bc6fb439af5963e42c4178b2
unknown <taewoo1212@gmail>
2024-04-02 20:18:27 +0900

    [feature] routes 주소연결, Login,Main,Instroduction 페이지 간단 연결 태욱

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,8 +1,29 @@
 export const navigationItems = [
   {
+    id: 'Main',
     path: '/',
-    element: '',
-    index: true,
-    withAuthorization: false,
+    text: '메인 페이지',
+    lazy: async () => {
+      const Module = await import('@/pages/Main');
+      return { Component: Module.default };
+    },
+  },
+  {
+    id: 'Login',
+    path: '/Login',
+    text: '로그인 페이지',
+    lazy: async () => {
+      const Module = await import('@/pages/Login');
+      return { Component: Module.default };
+    },
+  },
+  {
+    id: 'Introduction',
+    path: '/Introduction',
+    text: '회사소개 페이지',
+    lazy: async () => {
+      const Module = await import('@/pages/Introduction');
+      return { Component: Module.default };
+    },
   },
 ];
