212015af692d24e68c23fb22f91fbea3b7833913
unknown <taewoo1212@gmail>
2024-04-05 15:42:56 +0900

    [Feat] PageTitle, PageMainMenu 합치기 헤더 + 푸터 컴포넌트 만들기 전 태욱

--- src/pages/Introduction/Introduction.tsx
+++ src/pages/Introduction/Introduction.tsx
@@ -5,28 +5,34 @@ import mainImage from '/introduction_main.svg';
 import { StyledPageMainImageWrapper } from '@/styles/components/PageMainImageWrapper/PageMainImageWrapper';
 import MainMenuButton from '@/components/moleclues/MainMenuButton/MainMenuButton';
 import PageMainImage from '@/components/atoms/PageMainImage/PageMainImage';
+import PageTitle from '@/components/atoms/PageTitle/PageTitle';
+import NaviBar from '@/components/moleclues/naviButton/NaviBar';
 
 function Introduction() {
   return (
-    <Wrapper>
-      <div className="mainMenuPosition">
-        <MainMenuButton isHeader={false} />
-      </div>
-      <StyledPageMainImageWrapper>
-        {/* <img className="mainImage" src={mainImage} alt="회사소개 페이지" /> */}
-        <PageMainImage image={mainImage} alt="회사소개 페이지"></PageMainImage>
-        <StyledFlexCenter>
-          <PageLayout
-            firstTitle="COMPANY"
-            secondTitle="회사소개"
-            thirdTitle="고객의 만족은 우리의 기쁨"
-            thirdSubTitle="나람사료"
-          >
-            <IntroductionContent />
-          </PageLayout>
-        </StyledFlexCenter>
-      </StyledPageMainImageWrapper>
-    </Wrapper>
+    <>
+      <NaviBar isHeader={false} />
+      <PageTitle EngTitle={'COMPANY'} KorTitle={'회사소개'} />
+      <Wrapper>
+        <div className="mainMenuPosition">
+          <MainMenuButton isHeader={false} />
+        </div>
+        <StyledPageMainImageWrapper>
+          {/* <img className="mainImage" src={mainImage} alt="회사소개 페이지" /> */}
+          <PageMainImage image={mainImage} alt="회사소개 페이지"></PageMainImage>
+          <StyledFlexCenter>
+            <PageLayout
+              firstTitle="COMPANY"
+              secondTitle="회사소개"
+              thirdTitle="고객의 만족은 우리의 기쁨"
+              thirdSubTitle="나람사료"
+            >
+              <IntroductionContent />
+            </PageLayout>
+          </StyledFlexCenter>
+        </StyledPageMainImageWrapper>
+      </Wrapper>
+    </>
   );
 }
 
