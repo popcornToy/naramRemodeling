f23a339d8bb1247357a246c4547ceb2ba7e9e91d
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-04-07 14:54:37 +0900

    [Feat] 메인페이지 작업
    - 메인페이지 작업중

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,15 @@
 import NaviBar from '@/components/moleclues/naviButton/NaviBar';
 import mainBanner from '/mainBanner.png';
+import { MainBannerContainer } from '@/styles';
 
 export default function Main() {
   return (
     <>
       <header>
         <NaviBar isHeader color="black" justifyContent="end" />
-        <img src={mainBanner} alt="메인 이미지" />
+        <MainBannerContainer>
+          <img src={mainBanner} alt="메인 이미지" />
+        </MainBannerContainer>
       </header>
 
       <div>
