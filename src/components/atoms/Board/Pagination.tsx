53642e0510d37ea350e1a922e94f4aed44774921
iamjinpark <wldls7754@gmail.com>
2024-04-11 18:01:34 +0900

    [feat] 페이지네이션 UI 구현 완료

--- src/components/atoms/Board/Pagination.tsx
+++ src/components/atoms/Board/Pagination.tsx
@@ -1,48 +1,24 @@
-// import { useState } from 'react';
-// import PaginationComponent from 'react-js-pagination';
+import { StyledPagination } from '@/styles/components/Board/Pagination';
 
-// type PaginationProps = {
-//   activePage: number;
-//   itemsCountPerPage: number;
-//   totalItemsCount: number;
-//   pageRangeDisplayed: number;
-//   prevPageText: string;
-//   nextPageText: string;
-//   onChange: (page: number) => void;
-// };
+type pagination = {
+  handlePageClick: any;
+};
 
-// const Pagination = () => {
-//   const [page, setPage] = useState(1);
-
-//   const handlePageChange = (page: number) => {
-//     setPage(page);
-//   };
-
-//   return (
-//     <PaginationComponent
-//       activePage={page} // 현재 페이지
-//       itemsCountPerPage={10} // 한 페이지랑 보여줄 아이템 갯수
-//       totalItemsCount={450} // 총 아이템 갯수
-//       pageRangeDisplayed={5} // paginator의 페이지 범위
-//       prevPageText={'‹'} // "이전"을 나타낼 텍스트
-//       nextPageText={'›'} // "다음"을 나타낼 텍스트
-//       onChange={handlePageChange} // 페이지 변경을 핸들링하는 함수
-//     />
-//   );
-// };
-
-// export default Pagination;
-
-function Pagination() {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-
+const Pagination = ({ handlePageClick }: pagination) => {
   return (
-    <>
-      <div>1 | 2 | 3 | 4 | 5 </div>
-    </>
+    <StyledPagination
+      pageCount={Math.ceil(100 / 10)}
+      pageRangeDisplayed={3}
+      breakLabel="..."
+      previousLabel={'이전'}
+      nextLabel={'다음'}
+      onPageChange={handlePageClick}
+      containerClassName={'pagination-ul'}
+      activeClassName={'currentPage'}
+      previousClassName={'pageLabel-btn'}
+      nextClassName={'pageLabel-btn'}
+    />
   );
-}
+};
 
 export default Pagination;
